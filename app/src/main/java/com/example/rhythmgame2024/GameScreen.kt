db90a0f02bbb16f94f5aab2bcfec3ccc255a4764
EvaWu0126 <wwq2026@gmail.com>
2024-05-03 09:58:47 -0700

    Added basic gamepanel set up (not working yet...)

---
+++ app/src/main/java/com/example/rhythmgame2024/GameScreen.kt
@@ -0,0 +1,15 @@
+package com.example.rhythmgame2024
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(GamePanel(this));
+
+
+    }
+
+}
