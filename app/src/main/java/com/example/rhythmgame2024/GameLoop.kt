dfa839a62da59b7cc7ae49f7d1528275919df761
EvaWu0126 <wwq2026@gmail.com>
2024-05-07 09:59:24 -0700

    Created GameLoop class file, added basic gameloop funtion into GamePanel but currently not working (does not effect running)

---
+++ app/src/main/java/com/example/rhythmgame2024/GameLoop.kt
@@ -0,0 +1,26 @@
+package com.example.rhythmgame2024
+
+import android.util.Log
+import kotlin.math.log
+
+class GameLoop(gamePanel: GamePanel) : Runnable{
+
+    lateinit var gameThread: Thread
+    lateinit var gamePanel : GamePanel
+    public fun Gameloop(gamePanel: GamePanel) {
+        this.gamePanel = gamePanel
+        gameThread = Thread(this)
+    }
+
+    override fun run() {
+        while (true){
+            Log.d("GameLoop", "run: gameLoop rendering")
+            gamePanel.render()
+        }
+    }
+
+    public fun startGameLoop(){
+        gameThread!!.start()
+        Log.d("GameLoop", "startGameLoop: true")
+    }
+}
