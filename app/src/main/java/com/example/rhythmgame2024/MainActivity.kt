2dd89302b31b6e14c03294fdebde28af098dfb1b
v <viennatan21@gmail.com>
2024-05-07 08:49:00 -0700

    make binding in Main Activity

--- app/src/main/java/com/example/rhythmgame2024/MainActivity.kt
+++ app/src/main/java/com/example/rhythmgame2024/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.rhythmgame2024
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rhythmgame2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "MainActivity"
+    }
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
     }
 }
