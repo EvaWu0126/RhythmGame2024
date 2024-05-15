bbbbe8b2e4fe74ca92af6b4e25327545f295c7c5
EvaWu0126 <wwq2026@gmail.com>
2024-05-15 10:49:09 -0700

    json file +  Features

--- app/src/main/java/com/example/rhythmgame2024/Features.kt
+++ app/src/main/java/com/example/rhythmgame2024/Features.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Features(val name: String,
-                    val link: String,
                     val author: String,
                     val highScore: Int = 0,
-                    val combo: Int = 0) : Parcelable
+                    val combo: Int = 0,
+                    val beatmap: List<List<Int>>) : Parcelable
