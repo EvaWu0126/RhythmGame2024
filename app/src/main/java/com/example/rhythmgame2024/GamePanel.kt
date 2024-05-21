0b1966479a57e3158c3ad0eb76cd982b027c0bc5
EvaWu0126 <wwq2026@gmail.com>
2024-05-21 10:06:56 -0700

    beat in render is working, PointF list no working??

--- app/src/main/java/com/example/rhythmgame2024/GamePanel.kt
+++ app/src/main/java/com/example/rhythmgame2024/GamePanel.kt
@@ -10,9 +10,8 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import androidx.core.graphics.get
-import androidx.core.graphics.set
 import com.example.rhythmgame2024.entities.GameBeats
+
 
 class GamePanel(context: Context?, beatmap : List<List<Int>>) : SurfaceView(context), SurfaceHolder.Callback {
     private val redPaint = Paint()
@@ -21,11 +20,38 @@ class GamePanel(context: Context?, beatmap : List<List<Int>>) : SurfaceView(cont
 
     private val rawBeat : List<List<Int>> = beatmap
 
+    // beatList is not initialized???
+    private lateinit var beatList : MutableList<PointF>
+
+
 
     init {
         holder.addCallback(this)
         redPaint.color = Color.RED
 //        makeBeats()
+
+        var x : Float = 0F
+        for(row in rawBeat.indices) {
+            for (col in rawBeat[row].indices) {
+                if(rawBeat[row][col] == 1){
+                    if(col == 0){
+                        x = 240F
+                    }else if(col == 1){
+                        x = 540F
+                    }else if(col == 2){
+                        x = 840F
+                    }else if(col == 3){
+                        x = 1140F
+                    }else if(col == 4){
+                        x = 1440F
+                    }else if(col == 5){
+                        x = 1740F
+                    }
+                }
+                beatList.add(PointF(x, 0F))
+            }
+        }
+        Log.d("TAG", "Init: ${beatList}")
 
 
     }
@@ -55,37 +81,59 @@ class GamePanel(context: Context?, beatmap : List<List<Int>>) : SurfaceView(cont
         c.drawBitmap(GameBeats.BEATTAP5.tapCheck, 1470F,730F,null)
         c.drawBitmap(GameBeats.BEATTAP6.tapCheck, 1770F,730F,null)
 
-        for(row in rawBeat) {
-            for (col in row) {
-                if(row[col] == 1){
+        Log.d("beat", "rende_rawBeat: ${rawBeat}")
+        for(row in rawBeat.indices) {
+            Log.d("beat", "render_row: ${row}")
+            for (col in rawBeat[row].indices) {
+                Log.d("beat", "render_col: ${col}")
+                if(rawBeat[row][col] == 1){
                     if(col == 0){
                         c.drawBitmap(GameBeats.BEAT1.beats, 240F, 0F, null)
+                        Log.d("beat", "render: beat 1")
                     }else if(col == 1){
                         c.drawBitmap(GameBeats.BEAT2.beats, 540F, 0F, null)
+                        Log.d("beat", "render: beat 2")
                     }else if(col == 2){
                         c.drawBitmap(GameBeats.BEAT3.beats, 840F, 0F, null)
+                        Log.d("beat", "render: beat 3")
                     }else if(col == 3){
                         c.drawBitmap(GameBeats.BEAT4.beats, 1140F, 0F, null)
+                        Log.d("beat", "render: beat 4")
                     }else if(col == 4){
                         c.drawBitmap(GameBeats.BEAT5.beats, 1440F, 0F, null)
+                        Log.d("beat", "render: beat 5")
                     }else if(col == 5){
                         c.drawBitmap(GameBeats.BEAT6.beats, 1740F, 0F, null)
+                        Log.d("beat", "render: beat 6")
                     }
                 }
             }
         }
 
 
+
         // draw text function
         //c.drawText("Flos", 100F,100F, redPaint)
 
         holder.unlockCanvasAndPost(c)
+
     }
+
+//    fun update(delta: Int){
+//        for (pos in beatList.indices) {
+//            (pos.y += delta * 300).toFloat()
+//            if (pos.y >= 1500) {
+//                pos.y = 0f
+//            }
+//
+//        }
+//    }
 
 
 
     override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
         render(0F)
+        //update(20)
 //        gameloop.startGameLoop()
         Log.d("GamePanel", "GamePanel: surfacecreated")
 //        makeBeats()
